5c7934f62a3165abff8f070460bc372e4737d207
Will Braswell <wbraswell@hush.com>
2014-08-20 18:16:41 -0500

    Namespace Hack, Debug Broken Function Binding Part 8

--- bin/development/inline_cpp_debug.pl
+++ bin/development/inline_cpp_debug.pl
@@ -1,28 +1,25 @@
 #!/usr/bin/perl
 
-# AUTOMATIC CLASSES OPTION CAUSES SUBROUTINE CALL TO FAIL
-#use Inline (CPP => 'DATA', classes => sub { @_ = split( '__', shift ); ( pop, join( '::', @_ ) ); } );
+# AUTOMATIC CLASSES OPTION
+#use Inline (CPP => 'DATA', classes => sub { join('::', split('__', shift)); } );
 
-# MANUAL CLASSES OPTION ALLOWS SUBROUTINE AND METHOD CALLS TO SUCCEED
+# MANUAL CLASSES OPTION
 use Inline (CPP => 'DATA', classes => { 'RPerl__Test__Inline_CPP_Debug' => 'RPerl::Test::Inline_CPP_Debug'} );
 
 # SUBROUTINE IS ALWAYS IN SYMBOL TABLE: "have \%main:: symbol table entry for my_subroutine() = '*main::my_subroutine'"
 print STDERR q{in inline_cpp_debug.pl, have \%main:: symbol table entry for my_subroutine() = '} . (\%main::)->{'my_subroutine'} . "'\n" or croak();
 
-# METHOD CALL ALWAYS SUCCEEDS: "have $my_object->my_method() = 'RETVAL FROM my_subroutine()'"
+# METHOD CALL
 my $my_object = RPerl::Test::Inline_CPP_Debug->new();
 print STDERR "in inline_cpp_debug.pl, have \$my_object->my_method() = '" . $my_object->my_method() . "'\n" or croak();  # OO INTERFACE
 
-# SUBROUTINE CALL FAILS WITH AUTOMATIC CLASSES OPTION: "Undefined subroutine &main::my_subroutine called"
-#print STDERR q{in inline_cpp_debug.pl, have RPerl::Test::my_subroutine() = '} . RPerl::Test::my_subroutine() . "'\n" or croak();
-#print STDERR q{in inline_cpp_debug.pl, have $my_object->my_subroutine() = '} . $my_object->my_subroutine() . "'\n" or croak();
+# SUBROUTINE CALL
 print STDERR q{in inline_cpp_debug.pl, have my_subroutine() = '} . my_subroutine() . "'\n" or croak();
 
 __DATA__
 __CPP__
 SV* my_subroutine() { return(newSVpv("RETVAL FROM my_subroutine()", 25)); }
 
-// DELETING C++ CLASS BELOW (AND OBJECT/METHOD CALLS IN PERL ABOVE) ALLOWS SUBROUTINE CALL TO SUCCEED, REGARDLESS OF CLASSES OPTION
 class RPerl__Test__Inline_CPP_Debug
 {
 public:
--- lib/RPerl/Inline.pm
+++ lib/RPerl/Inline.pm
@@ -4,19 +4,16 @@ use RPerl;
 #use Config;
 
 # long form
-#use Inline CPP => config => classes => sub {
+#use Inline CPP => config => classes =>
+#sub {
 #    my $class = shift;
 #    my @class_split = split('__', $class);
-#    my $class_auto = pop(@class_split);
-#    my $pkg_auto = join('::', @class_split);
-#    return($pkg_auto, $class_auto);
+#    my $class_join = join('::', @class_split);
+#    return($class_join);
 #};
 
-# medium form
-#use Inline CPP => config => classes => sub { my @c = split('__', shift); (pop(@c), join('::', @c)); };
-
 # short form
-#use Inline CPP => config => classes => sub { @_ = split('__', shift); (pop, join('::', @_)); };
+#use Inline CPP => config => classes => sub { join('::', split('__', shift)); };
 
 our @ARGS = (
     typemaps => "$RPerl::INCLUDE_PATH/typemap.rperl",
@@ -39,6 +36,6 @@ our @ARGS = (
         '#include <vector>',
         '#include <unordered_map>', # DEV NOTE: unordered_map may require '-std=c++0x' in CCFLAGS above
         ],
-    classes => sub { @_ = split( '__', shift ); ( pop, join( '::', @_ ) ); }
+    classes => sub { join('::', split('__', shift)); }
 );
 1;
