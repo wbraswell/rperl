cda1017ff581e62e5d890ddacc71f473f110db1c
Will Braswell <wbraswell@hush.com>
2014-10-12 21:39:35 -0500

    Grammar, EmptyClass_00 %properties

--- lib/RPerl/Test/EmptyClass_00.pm
+++ lib/RPerl/Test/EmptyClass_00.pm
@@ -8,18 +8,18 @@ our $VERSION = 0.001_000;
 use parent qw(RPerl::Test);
 use RPerl::Test;
 
-# START HERE: debug %properties parsing, then create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
-# START HERE: debug %properties parsing, then create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
-# START HERE: debug %properties parsing, then create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values and print operator
+
+# START HERE: create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
+# START HERE: create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
+# START HERE: create EmptyPackage_0* and EmptyClass_0* to test version numbers, etc.
 
 # [[[ OO PROPERTIES ]]]
 our %properties = ( ## no critic qw(ProhibitPackageVars)  # USER DEFAULT 2: allow OO properties
-
-    #    empty_property => my integer $TYPED_empty_property           = 23,
-    #    empty_property => 23
+    empty_property => my integer $TYPED_empty_property = 23
 );
 
 our void $foo = sub { 2; };
 
 1;
-1;    # CODE SEPARATOR: methods above, subroutines below
+1;                  # CODE SEPARATOR: methods above, subroutines below
