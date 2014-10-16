515214c4fdd427f06283d27c6ea151d2c41ac344
Will Braswell <wbraswell@hush.com>
2014-10-16 05:53:46 -0500

    Grammar, Version Number Test Modules

---
+++ lib/RPerl/Test/EmptyModule/Package_00_Bad_VersionNumber_03.pm
@@ -0,0 +1,11 @@
+# [[[ HEADER ]]]
+package RPerl::Test::EmptyModule::Package_00_Good;
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_00;
+
+our void $empty_sub = sub { 2; };
+
+1;
+1;    # CODE SEPARATOR: end of package
