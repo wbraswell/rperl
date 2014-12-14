a6656b479404f695779136cea6ebfb454da755bc
Will Braswell <william.braswell@autoparallel.com>
2014-12-13 22:08:24 -0600

    Interpret & Run Test, Geometric Algebra IPBVE Part 1

---
+++ lib/RPerl/Test/AlgorithmMath/geometric_algebra_ipbve_good_00.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.000_002;
+
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values and print operator
+## no critic qw(ProhibitUnreachableCode RequirePodSections RequirePodAtEnd PodSpelling) # DEVELOPER DEFAULT 1: allow unreachable & POD-commented code
+
+use RPerl::Algorithm::Math::GeometricAlgebra;
+use Data::Dumper;
+
+# variable declarations
+my number__array_ref $retval_vector;
+my number__array_ref $input_bivector;
+my number__array_ref $input_vector;
+
+# [ undef, -10.1538461538462, 3, 1.53846153846154 ];
+$input_bivector = [-999999, 3.0/13.0, 4.0/13.0, 12.0/13.0];
+$input_vector = [-999999, 3.0, 4.0, 12.0];
+
+# error 1
+#$input_bivector = [-999999, 3.0, 4.0];
+#$input_vector = [-999999, 3.0, 4.0, -12.0];
+
+# error 1
+#$input_bivector = [-999999, 3.0, 4.0, 12.0, 0.0];
+#$input_vector = [-999999, 3.0, 4.0, -12.0];
+
+# error 2
+#$input_bivector = [-999999, 3.0, 4.0, 12.0];
+#$input_vector = [-999999, 3.0, 4.0];
+
+# error 2
+#$input_bivector = [-999999, 3.0, 4.0, 12.0];
+#$input_vector = [-999999, 3.0, 4.0, -12.0, 0.0];
+
+
+# loop to test for memory leaks
+my const_integer $i_MAX = 0;
+for my integer $i ( 0 .. $i_MAX ) {
+	RPerl::diag "in geometric_algebra_test_2_ipbve.pl, top of for() loop $i/$i_MAX\n" or croak();
+
+    $retval_vector = inner_product__bivector_vector_euclidean($input_bivector, $input_vector);
+
+	RPerl::diag "in geometric_algebra_test_2_ipbve.pl $i/$i_MAX, have \$retval_vector =\n" . Dumper($retval_vector) . "\n";
+
+    # root sum squared
+#    my number $checksum = (($retval_vector->[1])**2 + ($retval_vector->[2])**2 + ($retval_vector->[3])**2)**0.5;
+#    my number $checksum = (($retval_vector->[1] * $retval_vector->[1]) + ($retval_vector->[2] * $retval_vector->[2]) + ($retval_vector->[3] * $retval_vector->[3]))**0.5;
+#    RPerl::diag "in geometric_algebra_test_2_ipbve.pl $i/$i_MAX, have \$checksum = $checksum\n";
+
+croak('Done for now, croaking');
+}
+
+#croak('Done for now, croaking');
