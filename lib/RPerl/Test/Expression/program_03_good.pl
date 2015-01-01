1cb22938f6792e215a39f431c77722606637ef79
Will Braswell <william.braswell@autoparallel.com>
2015-01-01 15:31:46 -0500

    Grammar, Expression Tests Part 1

---
+++ lib/RPerl/Test/Expression/program_03_good.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+our integer $foo = sub {
+    ( my string $bar, my integer $bat, my integer $baz) = @_;
+    print $bar, "\n";
+    return $bat + $baz;
+};
+foo('howdy!');
