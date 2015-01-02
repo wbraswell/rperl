046f44be5bb6c3fb679f8f1d28adf24d9dd3ae08
Will Braswell <william.braswell@autoparallel.com>
2015-01-02 01:34:27 -0500

    Grammar, Expression Tests Part 2

--- lib/RPerl/Test/Foo.pm
+++ lib/RPerl/Test/Foo.pm
@@ -1,2 +1,33 @@
+# [[[ HEADER ]]]
 package RPerl::Test::Foo;
-1;
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ OO INHERITANCE ]]]
+use parent qw(RPerl::Test);
+use RPerl::Test;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OO OBJECT PROPERTIES ]]]
+our %properties = ( ## no critic qw(ProhibitPackageVars)  # USER DEFAULT 3: allow OO properties
+    plugh => my integer $TYPED_plugh           = 23,
+);
+
+# [[[ OO METHODS ]]]
+
+our void__method $quux = sub {
+    ( my object $self) = @_;
+    $self->{plugh} = $self->{plugh} * 2;
+};
+
+our string__method $corge = sub {
+    ( my object $self) = @_;
+    return $self->{xyzzy} x 3;
+};
+
+1;                          # end of class
