f853c7643f2abe9e2f21836df6a37c42f9f6bd97
Will Braswell <william.braswell@autoparallel.com>
2015-01-31 23:57:47 -0600

    OO Properties, Test $properties, Part 1

---
+++ lib/RPerl/Test/Properties/Class_00_Good.pm
@@ -0,0 +1,23 @@
+# [[[ HEADER ]]]
+package RPerl::Test::Properties::Class_00_Good;
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ OO INHERITANCE ]]]
+use parent qw(RPerl::Test);
+use RPerl::Test;
+
+# [[[ OO PROPERTIES ]]]
+our hash_ref $properties
+    = { test_property => my integer $TYPED_test_property = 2 };
+
+# [[[ OO METHODS ]]]
+our void__method $test_method = sub {
+    ( my object $self, my integer $input_integer) = @_;
+    $self->{test_property} *= $input_integer;
+    return $self->{test_property};
+};
+
+1;    # end of class
---
+++ lib/RPerl/Test/Properties/class_00_good_00.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< RUN_SUCCESS: '46' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.000_010;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+
+# [[[ INCLUDES ]]]
+use RPerl::Test::Properties::Class_00_Good;
+
+# [[[ OPERATIONS ]]]
+my $test_object = RPerl::Test::Properties::Class_00_Good->new();
+my $retval = $test_object->test_method(23);
+print $retval . "\n";
