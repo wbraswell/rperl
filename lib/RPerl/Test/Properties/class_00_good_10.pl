10791f5c90011f184f303f04f4a1e8304ba13b76
Will Braswell <william.braswell@autoparallel.com>
2015-02-07 23:03:40 -0600

    OO Properties, Test $properties, Part 6

---
+++ lib/RPerl/Test/Properties/class_00_good_10.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< RUN_SUCCESS: '46' >>>
+# <<< RUN_SUCCESS: '48' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.000_010;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+
+# [[[ INCLUDES ]]]
+use RPerl::Test::Properties::Class_00_Good;
+
+# [[[ OPERATIONS ]]]
+# do not set object property ourself, just let test_method() do it for us
+my object $test_object = RPerl::Test::Properties::Class_00_Good->new();
+my integer $retval = $test_object->test_method(23);
+print $retval . "\n";
+
+$retval = $test_object->test_method(24);
+print $retval . "\n";
---
+++ lib/RPerl/Test/Properties/class_00_good_11.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< RUN_SUCCESS: '69' >>>
+# <<< RUN_SUCCESS: '72' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.000_010;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+
+# [[[ INCLUDES ]]]
+use RPerl::Test::Properties::Class_00_Good;
+
+# [[[ OPERATIONS ]]]
+# set and get object property ourself
+# using auto-generated set_test_property() and get_test_property() methods, preserving object encapsulation
+my object $test_object = RPerl::Test::Properties::Class_00_Good->new();
+$test_object->set_test_property(3);
+$test_object->test_method(23);
+print $test_object->get_test_property() . "\n";
+
+$test_object->test_method(24);
+print $test_object->get_test_property() . "\n";
