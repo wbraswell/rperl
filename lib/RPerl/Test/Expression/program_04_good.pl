d80b2f1a33d87eecea6b188698c090cbd61679fc
Will Braswell <william.braswell@autoparallel.com>
2015-02-18 23:52:34 -0600

    Class Methods, Decide To Remove Yet Again

--- lib/RPerl/Test/Expression/program_04_good.pl
+++ lib/RPerl/Test/Expression/program_04_good.pl
@@ -16,9 +16,7 @@ use RPerl::Test::Foo;
 #RPerl::Test::Foo::grault(17);
 grault(17);
 
-# IS THERE REALLY ANY DIFFERENCE BETWEEN A SUBROUTINE AND A CLASS METHOD?
-# DELETE CLASS METHODS YET AGAIN???
 
-# START HERE 2: flesh Foo from ClassTemplate, implement class method tests (will be "static" method in C++) & object method tests
-# START HERE 2: flesh Foo from ClassTemplate, implement class method tests (will be "static" method in C++) & object method tests
-# START HERE 2: flesh Foo from ClassTemplate, implement class method tests (will be "static" method in C++) & object method tests
+# START HERE 2: remove all class method code, add feature bounty for class methods (static methods in C++); flesh Foo from ClassTemplate, implement object method tests
+# START HERE 2: remove all class method code, add feature bounty for class methods (static methods in C++); flesh Foo from ClassTemplate, implement object method tests
+# START HERE 2: remove all class method code, add feature bounty for class methods (static methods in C++); flesh Foo from ClassTemplate, implement object method tests
