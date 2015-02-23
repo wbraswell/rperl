c6ae340df349900417438fb4fb12f24d63fb1131
Will Braswell <william.braswell@autoparallel.com>
2015-02-22 21:31:58 -0600

    Class Methods, Remove Code, Part 1

--- lib/RPerl/CompileUnit/Module/ClassTemplate.pm
+++ lib/RPerl/CompileUnit/Module/ClassTemplate.pm
@@ -104,19 +104,6 @@ our string__method $corge = sub {
     return $self->{xyzzy} x 3;
 };
 
-# [[[ OO CLASS METHODS ]]]
-
-# <<< CHANGE_ME: delete for no methods, or replace with real method(s) >>>
-our void__method $xuuq = sub {
-    ( my string $class) = @_;
-    print 'Class method xuuq called from class ', $class, "\n";
-};
-
-our number__method $egroc = sub {
-    ( my string $class, my number $hgulp) = @_;
-    return $hgulp * 3;
-};
-
 # [[[ SUBROUTINES ]]]
 
 # <<< CHANGE_ME: delete for no subroutines, or replace with real subroutine(s) >>>
