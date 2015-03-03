618753a99b968eb25fb6e424944017f97db5f47b
Will Braswell <william.braswell@autoparallel.com>
2015-03-02 23:58:48 -0600

    String Repeat Tests, Part 1

---
+++ lib/RPerl/Test/Operator07StringRepeat/program_00_good.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< RUN_SUCCESS: "have $foo = ''" >>>
+# <<< RUN_SUCCESS: "have $bar = 'bar'" >>>
+# <<< RUN_SUCCESS: "have $bat = 'batbat'" >>>
+# <<< RUN_SUCCESS: "have $baz = 'bazbazbazbaz'" >>>
+# <<< RUN_SUCCESS: "have $bax = 'baxbaxbaxbaxbaxbaxbaxbax'" >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my string $foo = 'foo' x 0;
+my string $bar = 'bar' x 1;
+my string $bat = 'bat' x 2;
+my string $baz = 'baz' x 4;
+my string $bax = 'bax' x 8;
+
+print q{have $foo = '}, $foo, q{'}, "\n";
+print q{have $bar = '}, $bar, q{'}, "\n";
+print q{have $bat = '}, $bat, q{'}, "\n";
+print q{have $baz = '}, $baz, q{'}, "\n";
+print q{have $bax = '}, $bax, q{'}, "\n";
+
+# START HERE 3: add string repeat failure tests
+# START HERE 3: add string repeat failure tests
+# START HERE 3: add string repeat failure tests
