909a8069fac818800c114f866c4c945125822d5e
Will Braswell <william.braswell@autoparallel.com>
2015-03-08 23:59:25 -0500

    Constant Value Tests, Part 1

---
+++ lib/RPerl/Test/Constant/program_01_good.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< RUN_SUCCESS: '1.570795' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CONSTANTS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(ProhibitConstantPragma ProhibitMagicNumbers)  # USER DEFAULT 3: allow constants
+use constant PI  => my number $TYPED_PI  = 3.141_59;
+use constant PIE => my string $TYPED_PIE = 'pecan';
+
+# [[[ OPERATIONS ]]]
+my number $pi_over_2 = PI() / 2;
+print 'pi / 2 = ', $pi_over_2, "\n";
