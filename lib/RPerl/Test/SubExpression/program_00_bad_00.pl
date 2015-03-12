8184f26ec72cc2159ef95f6b7963e6c434fbc984
Will Braswell <william.braswell@autoparallel.com>
2015-03-11 23:59:01 -0500

    SubExpression Tests, Part 2

---
+++ lib/RPerl/Test/SubExpression/program_00_bad_00.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< COMPILE_ERROR: 'ERROR ECVPAPL02' >>>
+# <<< COMPILE_ERROR: 'Bareword "undefined" not allowed' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my integer $foo = undefined;
