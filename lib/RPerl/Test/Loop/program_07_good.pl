d84616568f5b6a6aeb1a1e9169ab258bbe72d336
Will Braswell <william.braswell@autoparallel.com>
2015-03-21 22:46:51 -0500

    Loop Tests, Part 3

---
+++ lib/RPerl/Test/Loop/program_07_good.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my integer $i = 3;
+while ( $i > -5 ) {
+    print 'Production rule Loop matched by LoopWhile, iteration number ', $i,
+        "\n";
+    $i--;
+}
---
+++ lib/RPerl/Test/Statement/program_03_good.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my integer $i = 10;
+while ( $i < 23 ) {
+    print 'Production rule Statement matched by Loop, iteration number ', $i,
+        "\n";
+    $i += 2;
+}
