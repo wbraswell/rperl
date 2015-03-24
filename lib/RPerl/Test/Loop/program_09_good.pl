f32e3b74e88c6e80a94236358f627dffed4c30c7
Will Braswell <william.braswell@autoparallel.com>
2015-03-23 23:31:06 -0500

    Loop Tests, Part 6

--- lib/RPerl/Test/Loop/program_09_good.pl
+++ lib/RPerl/Test/Loop/program_09_good.pl
@@ -12,10 +12,10 @@ our $VERSION = 0.001_000;
 # [[[ OPERATIONS ]]]
 
 TESTFOREACHLOOP: foreach my number $my_number ( 1.1, 2.2, 3.3, 4.4 ) {
+    if ( $my_number == 3.3 ) {
+        next TESTFOREACHLOOP;
+    }
     print 'Production rule Loop matched by LoopForEach, iteration item ',
         $my_number,
         "\n";
-    if ( $my_number == 3.3 ) {
-        last TESTFOREACHLOOP;
-    }
 }
