cb099d82d3cd68c6952532f870645eb3c43d14c3
Will Braswell <william.braswell@autoparallel.com>
2015-03-27 00:31:02 -0500

    Variable Modification Tests, Part 2

--- lib/RPerl/Test/VariableModification/program_03_good.pl
+++ lib/RPerl/Test/VariableModification/program_03_good.pl
@@ -8,6 +8,7 @@ our $VERSION = 0.001_000;
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
 ## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+## no critic qw(ProhibitExplicitStdin)  # USER DEFAULT 4: allow <STDIN> prompt
 
 # [[[ OPERATIONS ]]]
 
