a4d5be0fb0cac943afb418eab9b700d2c3642fee
Will Braswell <william.braswell@autoparallel.com>
2015-03-26 23:18:28 -0500

    Variable Modification Tests

---
+++ lib/RPerl/Test/VariableModification/program_04_bad_00.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< COMPILE_ERROR: 'ERROR ECVPARP00' >>>
+# <<< COMPILE_ERROR: 'Unexpected token:  ]' >>>
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my integer__array_ref $i_array;
+$i_array = [ -20, -15, -10, -5, 0, 5, 10, 15, 20, ];
---
+++ lib/RPerl/Test/VariableModification/program_05_good.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+
+my string__hash_ref $s_hash;
+$s_hash = {
+    titans     => 'ccm',
+    redhawks   => 'msu',
+    redraiders => 'ttu',
+    bears      => 'bu'
+};
