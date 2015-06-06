af2be5da640d19de9f7d3251ba3484754957330a
Will Braswell <william.braswell@autoparallel.com>
2015-06-06 00:20:02 -0500

    Generator, Perl Ops Perl Types, OperatorVoid::Named::Croak, Part 3

--- lib/RPerl/Test/OperatorVoid01NamedVoidCroak/program_08_bad_00.pl
+++ lib/RPerl/Test/OperatorVoid01NamedVoidCroak/program_08_bad_00.pl
@@ -2,7 +2,7 @@
 
 # [[[ PREPROCESSOR ]]]
 # <<< PARSE_ERROR: 'ERROR ECVPARP00' >>>
-# <<< PARSE_ERROR: 'Unexpected Token:  }' >>>
+# <<< PARSE_ERROR: 'Unexpected Token:  <<< NO TOKEN FOUND >>>' >>>
 
 # [[[ HEADER ]]]
 use strict;
