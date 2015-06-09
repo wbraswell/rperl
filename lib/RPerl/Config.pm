900ae2defc1f0e253a760daaeb2e5b1b0623cd29
bulk88 <bulk88@hotmail.com>
2015-06-09 16:44:37 -0400

    select is only for sockets on win32, drive letter fixes

--- lib/RPerl/Config.pm
+++ lib/RPerl/Config.pm
@@ -394,10 +394,10 @@ for my $i ( 0 .. ( ( scalar @directories_rperl_pm_split ) - 1 ) ) {
 #print {*STDERR} 'in RPerl::Config, have @directories_rperl_split = ', "\n", Dumper(\@directories_rperl_split), "\n";
 
 # NEED FIX: how do we catpath() with some $volume instead of catdir() below, without breaking relative paths?
-$BASE_PATH = File::Spec->catdir(@directories_base_split);
+$BASE_PATH = File::Spec->catpath($volume_loaded, File::Spec->catdir(@directories_base_split));
 if ( $BASE_PATH eq q{} ) {
-    $INCLUDE_PATH = File::Spec->catdir(@directories_rperl_pm_split);
-    $SCRIPT_PATH  = File::Spec->catdir(@directories_rperl_split);
+    $INCLUDE_PATH = File::Spec->catpath($volume_loaded, File::Spec->catdir(@directories_rperl_pm_split));
+    $SCRIPT_PATH  = File::Spec->catpath($volume_loaded, File::Spec->catdir(@directories_rperl_split));
 #    print {*STDERR} 'in RPerl::Config, have $BASE_PATH eq q{} = ', $BASE_PATH, "\n";
 }
 else {
