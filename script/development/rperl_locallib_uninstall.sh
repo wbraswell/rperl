321ad01e2d8408cc3b005d901da3f0f9b908e6ea
Will Braswell <william.braswell@autoparallel.com>
2015-07-07 04:18:45 -0500

    CPAN Distribution, Add Meta Info To Makefile.PL

--- script/development/rperl_locallib_uninstall.sh
+++ script/development/rperl_locallib_uninstall.sh
@@ -9,11 +9,13 @@ rm -f rperl*
 cd ~/perl5/man/man3
 rm -f RPerl*
 
+# THESE DIRS MAY NOT EXIST, CAUSING ERROR MESSAGES
 cd ~/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/auto
 rm -Rf RPerl*
 cd ~/perl5/lib/perl5/x86_64-linux-gnu-thread-multi/.meta/
 rm -Rf RPerl*
 
+# THESE DIRS MAY NOT EXIST, CAUSING ERROR MESSAGES
 cd ~/perl5/lib/perl5/x86_64-linux/auto
 rm -Rf RPerl*
 cd ~/perl5/lib/perl5/x86_64-linux/.meta/
