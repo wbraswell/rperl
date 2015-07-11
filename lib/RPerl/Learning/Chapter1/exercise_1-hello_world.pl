8c3dec4448fbed6bba04f92f9f23759c5512f2e9
Will Braswell <william.braswell@autoparallel.com>
2015-07-10 23:20:16 -0500

    Learning RPerl, Chapter 1, Exercise 1, Hello World

---
+++ lib/RPerl/Learning/Chapter1/exercise_1-hello_world.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+
+# [[[ OPERATIONS ]]]
+print 'Hello, world!' . "\n";
