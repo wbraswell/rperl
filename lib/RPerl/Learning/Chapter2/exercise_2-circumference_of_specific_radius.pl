8c89f274b44590a4ab45c05b049e2ffa5b8135f5
Will Braswell <william.braswell@autoparallel.com>
2015-07-11 23:09:56 -0500

    Learning RPerl, Chapter 2, Exercise 1, Circumference Of Circle With Radius 12.5

---
+++ lib/RPerl/Learning/Chapter2/exercise_2-circumference_of_specific_radius.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(ProhibitConstantPragma ProhibitMagicNumbers)  # USER DEFAULT 3: allow constants
+
+use constant $PI = 3.141_592_654;
+
+# [[[ OPERATIONS ]]]
+my number $radius = 12.5;
+my number $circumference = 2 * PI() * $radius;
+
+print 'Pi = ' . PI() . "\n";
+print 'Radius = ' . $radius . "\n";
+print 'Circumference = ' . $circumference . "\n";
