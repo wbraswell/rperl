56ec0ee7c74596417b946f735f810bf43eb4f5b2
Will Braswell <william.braswell@autoparallel.com>
2015-07-16 21:05:59 -0500

    Learning RPerl, Chapter 2, Exercise 5, String Repeat

---
+++ lib/RPerl/Learning/Chapter2/exercise_5-string_repeat.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(ProhibitExplicitStdin)  # USER DEFAULT 4: allow <STDIN> prompt
+
+# [[[ OPERATIONS ]]]
+print 'Please input string to be repeated: ';
+my string $repeat_string = <STDIN>;
+
+print 'Please input integer (whole number) times to repeat string: ';
+my string $repeat_integer_string = <STDIN>;
+my integer $repeat_integer = string_to_integer($repeat_integer_string);
+
+print "\n";
+print ($repeat_string x $repeat_integer);
