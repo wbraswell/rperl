99ca70b1311bdc9a35022da735c4b9986659a7dc
Will Braswell <william.braswell@autoparallel.com>
2015-07-25 03:16:49 -0500

    Learning RPerl, Chapter 4, Exercise 3, Subroutines For Above-Average Array Elements

---
+++ lib/RPerl/Learning/Chapter4/exercise_3-subroutine_above_average.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+# Learning RPerl, Chapter 4, Exercise 3
+# Subroutines & driver to calculate the above-average elements of hard-coded arrays
+
+# [[[ HEADER ]]]
+use strict;
+use warnings;
+use RPerl;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(ProhibitExplicitStdin)  # USER DEFAULT 4: allow <STDIN> prompt
+
+# [[[ SUBROUTINES ]]]
+
+our number $total = sub {
+    ( my number_arrayref $input_numbers ) = @_;
+    my number $retval = 0;
+    foreach my number $input_number ( @{$input_numbers} ) {
+        $retval += $input_number;
+    }
+    return $retval;
+};
+
+our number $average = sub {
+    ( my number_arrayref $input_numbers ) = @_;
+    return (total($input_numbers) / (scalar @{$input_numbers}));
+};
+
+our number_arrayref $above_average = sub {
+    ( my number_arrayref $input_numbers ) = @_;
+    my number $average = average($input_numbers);
+    my number_arrayref $retval = [];
+    foreach my number $input_number (@{$input_numbers}) {
+        if ($input_number > $average) {
+            push @{$retval}, $input_number;
+        }
+    }
+    return $retval;
+};
+
+# [[[ OPERATIONS ]]]
+
+my string_arrayref $fred = [1 .. 10];
+my number $fred_above_average = above_average($fred);
+print '$fred is ' . number_arrayref_to_string($fred) . "\n";
+print 'The above-average elements of $fred are ' . number_arrayref_to_string($fred_above_average) . "\n";
+print '(Should be [6, 7, 8, 9, 10])' . "\n\n";
+
+my string_arrayref $barney = [100, 1 .. 10];
+my number $barney_above_average = above_average($barney);
+print '$barney is ' . number_arrayref_to_string($barney) . "\n";
+print 'The above-average elements of $barney are ' . number_arrayref_to_string($barney_above_average) . "\n";
+print '(Should be just [100])' . "\n";
