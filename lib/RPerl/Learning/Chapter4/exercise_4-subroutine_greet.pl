86bb1ab83d716a92aaa4f22c35ca07abcb571d57
Will Braswell <william.braswell@autoparallel.com>
2015-10-22 11:23:39 -0500

    Learning RPerl, Ensure All Exercises Parse, Part 6

--- lib/RPerl/Learning/Chapter4/exercise_4-subroutine_greet.pl
+++ lib/RPerl/Learning/Chapter4/exercise_4-subroutine_greet.pl
@@ -17,7 +17,7 @@ our $VERSION = 0.001_000;
 our string $greet = sub {
     ( my string $name, my string $previous_name ) = @_;
     print 'Hi ' . $name . '!  ';
-    if ($previous_name eq '') {
+    if ($previous_name eq q{}) {
         print 'You are the first one here!' . "\n";
     }
     else {
@@ -28,7 +28,7 @@ our string $greet = sub {
 
 # [[[ OPERATIONS ]]]
 
-my string $previous_name = '';
+my string $previous_name = q{};
 $previous_name = greet('Fred', $previous_name);
 $previous_name = greet('Barney', $previous_name);
 $previous_name = greet('Wilma', $previous_name);
