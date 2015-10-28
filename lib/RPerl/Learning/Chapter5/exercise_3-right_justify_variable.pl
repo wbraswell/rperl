3e63bfdcee02f7aab0809ceafe0e4069546403af
Will Braswell <william.braswell@autoparallel.com>
2015-10-27 23:39:41 -0500

    Learning RPerl, Ensure All Exercises Parse, Part 11

--- lib/RPerl/Learning/Chapter5/exercise_3-right_justify_variable.pl
+++ lib/RPerl/Learning/Chapter5/exercise_3-right_justify_variable.pl
@@ -7,7 +7,7 @@
 use RPerl;
 use strict;
 use warnings;
-our $VERSION = 0.002_000;
+our $VERSION = 0.003_000;
 
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
@@ -15,31 +15,32 @@ our $VERSION = 0.002_000;
 
 # [[[ SUBROUTINES ]]]
 
-our void $right_justify_20 = sub {
+our void $right_justify_variable = sub {
     my string_arrayref $input_strings = [];
-    print 'Please input integer column width, then press <ENTER>, then press <CTRL-D>:' . "\n";
-    my integer $column_width = string_to_integer(<STDIN>);
- 
-    print 'Please input zero or more strings, separated by <ENTER>, ended by <CTRL-D>:' . "\n";
-    while (my string $input_string = <STDIN>) {
-        push @{$input_strings}, $input_string;
-    } 
+    print 'Please input integer column width, then press <ENTER>:' . "\n";
+    my string $column_width_string = <STDIN>;
+    my integer $column_width       = string_to_integer($column_width_string);
 
-    my integer $ruler_width_tens = 6;  # default to ruler line width 60
-    if ($column_width > 60) {
-        $ruler_width_tens = number_to_integer($column_width / 10) + 1;
+    print 'Please input zero or more strings, separated by <ENTER>, ended by <CTRL-D>:' . "\n";
+    while ( my string $input_string = <STDIN> ) {
+        push @{$input_strings}, $input_string;
+    }
+
+    my integer $ruler_width_tens = 6;    # default to ruler line width 60
+    if ( $column_width > 60 ) {
+        $ruler_width_tens = number_to_integer( $column_width / 10 ) + 1;
     }
 
     print "\n";
     print '1234567890' x $ruler_width_tens;
     print "\n";
 
-    foreach my $input_string (@{$input_strings}) {
-        print q{ } x (($column_width + 1) - (length $input_string));
+    foreach my string $input_string ( @{$input_strings} ) {
+        print q{ } x ( ( $column_width + 1 ) - ( length $input_string ) );
         print $input_string;
     }
 };
 
 # [[[ OPERATIONS ]]]
 
-right_justify_20();
+right_justify_variable();
