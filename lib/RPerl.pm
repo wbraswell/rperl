e29d10ae436ba833cfc31886f30bffca89d574b1
Will Braswell <william.braswell@autoparallel.com>
2015-11-01 21:36:58 -0600

    CPAN Release, v1.2; Codename Andromeda, Halloween Edition

--- lib/RPerl.pm
+++ lib/RPerl.pm
@@ -4,10 +4,10 @@ use strict;
 use warnings;
 
 # DEV NOTE, CORRELATION #rp16: RPerl's underscore-is-comma (not CPAN's underscore-is-beta) numbering scheme utilized here
-our $VERSION = 1.100_002;    # CODENAME JUPITER
+our $VERSION = 1.200_000;    # CODENAME ANDROMEDA
 
-#our $VERSION = 20150911;    # NON-RELEASE VERSION NUMBER, OFFICIAL LONGDATE
-#our $VERSION = 2015.254;    # NON-RELEASE VERSION NUMBER, OFFICIAL STARDATE
+#our $VERSION = 20151031;    # NON-RELEASE VERSION NUMBER, OFFICIAL LONGDATE
+#our $VERSION = 2015.304;    # NON-RELEASE VERSION NUMBER, OFFICIAL STARDATE
 
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
