378220a87a750ef8416474a1dd64681364575d55
Will Braswell <william.braswell@autoparallel.com>
2015-11-15 04:22:41 -0600

    Learning RPerl, Content, Part 4

--- lib/RPerl/Learning.pm
+++ lib/RPerl/Learning.pm
@@ -3,7 +3,7 @@ use RPerl;
 package RPerl::Learning;
 use strict;
 use warnings;
-our $VERSION = 0.001_000;
+our $VERSION = 0.002_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::CompileUnit::Module::Class);
@@ -172,7 +172,7 @@ Restricted Perl, The Optimizing Perl 5 Compiler
 =head2 How did RPerl get to be so popular?
 
     The RPerl team has been regularly promoting RPerl in a number of physical and digital venues, including but not limited to:
- 
+
     =over
     =item YAPC::NA  L<http://www.yapcna.org>
     =item Austin Perl Mongers  L<http://www.austin.pm>
@@ -208,7 +208,25 @@ Restricted Perl, The Optimizing Perl 5 Compiler
 
 =head2 How can I get RPerl?
 
-    You may find the latest major release of RPerl (stable) on CPAN:
+    Installing RPerl ranges from easy to difficult, depending on how well your operating system supports Perl and C++.
+    
+    On modern operating systems with good Perl support, such as Debian or Ubuntu GNU/Linux, you should be able to install RPerl system-wide by running the following command at your terminal command prompt:
+    
+    C<$ sudo cpan RPerl>
+
+    If RPerl is properly installed, you should see a short text message displayed when you type the following command:
+
+    C<$ rperl -v>
+
+    On operating systems with less Perl support, you may have to perform a number of steps to successfully install RPerl, with dry technical detail available in the INSTALL notes document:
+    
+    L<https://github.com/wbraswell/rperl/blob/master/INSTALL>
+
+    Unless you are an experienced programmer or system administrator, it is B<strongly> recommended you use the Xubuntu operating system.  You can download the Xubuntu ISO file at the link below, then use it to create a bootable DVD disc or USB flash drive, install Xubuntu onto any computer, and issue the C<$ sudo cpan RPerl> command as described above.
+
+    L<http://xubuntu.org/getxubuntu>
+
+    If you are interested in viewing the source code of RPerl itself, you may find the latest major release of RPerl (stable) on CPAN:
 
     L<https://metacpan.org/author/WBRASWELL>
 
@@ -231,9 +249,9 @@ Restricted Perl, The Optimizing Perl 5 Compiler
 =head2 How can I get support for RPerl?
 
     Official RPerl technical support is provided through Auto-Parallel Technologies, Inc.
-    
+ 
     To request more information, please send an e-mail to the following address:
-    
+ 
     william DOT braswell AT autoparallel DOT com
 
 =head2 Are there any other kinds of support?
@@ -249,7 +267,7 @@ Restricted Perl, The Optimizing Perl 5 Compiler
     L<https://github.com/wbraswell/rperl/issues>
 
     Although Github Issues is strongly preferred, the RPerl development team also supports the legacy CPAN ticket system:
-    
+ 
     L<https://rt.cpan.org/Public/Dist/Display.html?Name=RPerl>
 
 =head2 How do I make an RPerl program?
@@ -286,31 +304,121 @@ our $VERSION = 0.001_000;
 # [[[ OPERATIONS ]]]
 my number $foo = 21 + 12;
 my number $bar = 23 * 42;
-print 'have $foo = ', $foo, "\n";
-print 'have $bar = ', $bar, "\n";
+print 'have $foo = ' . $foo . "\n";
+print 'have $bar = ' . $bar . "\n";
 >
 
 =head2 What's inside that RPerl program?
 
-This program is separated by blank lines into 4 sections: shebang, header, critics, and operations.
+    This program is separated by blank lines into 4 sections: shebang, header, critics, and operations.  
 
-The "shebang" always contains one line and is short for "hash bang", referring to the two leading characters C<#!> of this line.  The octothorpe character C<#> (tic-tac-toe symbol) is called a pound sign when used on a telephone, and is called a hash (or more recently "hash tag") when used on a computer.  The exclamation point character C<!> is called a bang when used on a computer.  When appearing together as the first two characters in a plain text file, the hash and bang characters tell the operating system to run the immediately-following command (in this case the Perl interpreter located at F</usr/bin/perl>) and pass the remaining contents of the text file as input to the command.  In other words, if the first line of a plain text file is C<#!/usr/bin/perl>, then that file is a Perl program.
+    Other than the shebang and critics, all lines beginning with C<#> are comments and can be safely ignored or discarded without affecting the program.
 
-The header always contains 4 lines for an RPerl program file ending in .pl, and 5 lines for an RPerl module ending in .pm (covered later in Chapter 11).  The C<use> Perl keyword (AKA builtin operator) has 2 primary purposes: to load additional RPerl modules, and to enable RPerl "pragma" system configuration modes.  The C<use RPerl;> line is dual-purpose, it both loads the F<RPerl.pm> module and enables the special RPerl low-magic pragma.  The C<use strict;> and C<use warnings;> lines enable the most basic Perl pragmas to require decent programming practices.  The C<our $VERSION = 0.001_000;> line sets the numeric version number of this RPerl program.
+    The "shebang" section is required and always contains exactly 1 line and is short for "hash bang", referring to the two leading characters C<#!> of this line.  The octothorpe character C<#> (tic-tac-toe symbol) is called a pound sign when used on a telephone, and is called a hash (or more recently "hash tag") when used on a computer.  The exclamation point character C<!> is called a bang when used on a computer.  When appearing together as the first two characters in a plain text file, the hash and bang characters tell the operating system to run the immediately-following command (in this case the Perl interpreter located at F</usr/bin/perl>) and pass the remaining contents of the text file as input to the command.  In other words, if the first line of a plain text file is C<#!/usr/bin/perl>, then that file is a Perl program.
 
-# START HERE: critics & operations
-# START HERE: critics & operations
-# START HERE: critics & operations
+    The header section is required and always contains 4 lines for an RPerl program file ending in F<.pl>, or 5 lines for an RPerl module ending in F<.pm> (covered later in Chapter 11).  The C<use> Perl keyword (AKA builtin operator) has 2 primary purposes: to load additional RPerl modules, and to enable RPerl "pragma" system configuration modes.  The C<use RPerl;> line is dual-purpose, it both loads the F<RPerl.pm> module and enables the special RPerl low-magic pragma.  The C<use strict;> and C<use warnings;> lines enable the most basic Perl pragmas which in turn require decent programming practices by the human programmers.  The C<our $VERSION = 0.001_000;> line sets the numeric version number of this RPerl program.
 
+    The critics section is included as necessary and may contain 1 or more lines beginning with C<## no critic>, which disable the errors caused by the over-restrictive nature of some Perl::Critic policies.  There are currently 6 critics commands enabled for normal RPerl users, the first 2 of which are given in this example.  The C<USER DEFAULT 1> critic command allows the use of numeric values such as C<21> and C<12>, as well as the common C<print> command.  The C<USER DEFAULT 2> critic command allows the printing of C<'have $foo = '>, where a single-quoted C<'> string literal value contains the the C<$> dollar sigil (covered later in Chapter 2).
+
+    The operations section is required and contains 1 or more lines of general-purpose RPerl source code.  This is the main body of your program.  The 4 lines of source code in our example are used to perform some simple arithmetic and display the results.  The C<my number $foo = 21 + 12;> line declares a new variable named C<$foo> which will only contain floating-point numeric data, and which is initialized to contain the arithmetic result of numeric literal values C<21> plus C<12>.  The C<my number $bar = 23 * 42;> line does much the same thing, creating a new numeric variable named C<$bar> and initialized with C<23> times C<42>.  The C<print 'have $foo = ' . $foo . "\n";> and C<print 'have $bar = ' . $bar . "\n";> lines will display on screen (not send to paper printer) the labeled values of C<$foo> and C<$bar> respectively.  The C<.> dot operator is string concatenation, used in this example to create one string out of 3 parts so there is only 1 argument parameter passed to the C<print> command.  The "n" in the C<"\n"> double-quoted string literal values stands for "newline", which inserts a carriage return to place the next piece of printed data down on the following line.
 
 =head2 How do I compile RPerl?
-=head2 A whirlwind tour of RPerl
- 
+
+    Normal Perl source code is executed using a software mechanism known as interpretation, which is to say that Perl is an interpreted language and the F</usr/bin/perl> command is called the Perl interpreter.  The primary alternative to interpretation is compilation, so RPerl is a compiled subset of the Perl language and the F</usr/bin/rperl> command is called the RPerl compiler.
+
+    Like the Perl interpreter, the RPerl compiler accepts 2 different input file types: Perl programs which end in F<.pl> and Perl modules which end in F<.pm>.  Perl program files actually run and execute actions, optionally receiving some functionality from 1 or more Perl module files if specified.  Perl modules do not run or execute actions themselves, they only provide functionality which must in turn be called from a Perl program, or from another Perl module which eventually gets called by a Perl program.
+
+    B<I<CURRENT RPERL LIMITATION: RPerl modules are supported in full compile mode, but RPerl programs are supported in test interpret mode only, to be updated in a forthcoming RPerl release.>>
+
+    A list of all valid RPerl compiler options may be seen by issuing the C<$ rperl -?> command or following one of these links:
+
+    L<rperl>
+    L<https://metacpan.org/pod/distribution/RPerl/script/rperl>
+
+    To partially-compile-then-execute the preceeding RPerl example program in test mode, you may copy and paste the entire program (from shebang to second print) into a temporary file such as F</tmp/foobar.pl>, then execute the following command:
+
+    C<$ rperl -t /tmp/foobar.pl>
+
+The output of this example program should be:
+
+C<
+have $foo = 33
+have $bar = 966
+>
+
+    Please see chapter 11 for more information about compiling Perl modules.
+
+=head2 A Whirlwind Tour of RPerl
+
+=head3 Creator of RPerl, Will Braswell
+
+    Will Braswell does more than just create Perl compiler software, he is also very active in several other areas of life, including but not limited to:
+
+    =over
+    =item Church & Spirituality
+    =item Boy Scouts of America
+    =item Cane Juggling & Circus Performance
+    =item Linux Operating Systems
+    =item Charitable & Fraternal Organizations
+    =item Homeschooling & Higher Education
+    =item Astrophysics & Mathematics
+    =item Entrepreneuriship & Business
+    =item High-Performance Computing
+    =item Professional Space Exploration
+    =item Family Life
+    =back
+
+    These areas of interest are reflected in the tone and intention of RPerl.
+
+=head3 History of RPerl
+
+    The RPerl project officially began as a New Year's Resolution on January 1st, 2013.  Following the grand tradition of Perl creator "Saint" Larry Wall, RPerl version releases are often timed to coincide with major holidays.
+
+    After 1 year of work, RPerl v1.0beta1 was released on New Year's Day 2014, eventually followed by RPerl v1.0beta2 on Christmas 2014.
+
+    The much-anticipated RPerl v1.0 full release was made on US Independence Day 2015, and RPerl v1.2 came on Halloween 2015.
+
+    RPerl v1.3 is scheduled for release on Thanksgiving 2015, then RPerl v2.0beta1 on Christmas 2015, and so on.
+
+    RPerl v1.0 was funded through a Kickstarter campaign, then RPerl v1.2 and v1.3 were funded through a second Kickstarter campaign.  Work on the first 6 chapters of this book was funded through a grant from The Perl Foundation.
+
+=head3 Performance of RPerl
+
+    The question of I<"How fast is RPerl?"> does not have one simple answer; instead there are several factors and configuration modes to be taken into consideration.  A relatively detailed description of the performance and modes may be found at the following link:
+
+    L<http://rperl.org/performance_benchmarks.html>
+
+    The most condensed answer is that I<"RPerl is really fast.">  Utilizing RPerl's fastest execution modes, we see performance very close to C++, which means RPerl is now among the short list of "world's fastest languages" along with C, C++, and Fortran.
+
+=head3 Low-Magic Perl Commandments
+
+    The high-magic (very complex) features of Perl are primarily responsible for how slow Perl runs for general-purpose computations.  The "R" in RPerl stands for "Restricted", in that we restrict ourselves to only use the low-magic features of Perl which can run really fast.
+
+    The definitive list of do's and don'ts for high-magic vs low-magic Perl programming is called The Low Magic Perl Commandments (LMPC).  There are 64 total commandments split into 5 groups of Ideals, Magic, Data, Operations, and Object-Orientation.  The "Thou Shalt" commandments appear in the left column, and the "Thou Shalt Nots" appear on the right.
+
+    L<http://rperl.org/the_low_magic_perl_commandments.html>
+
+    The LMPC draw inspiration from, and (wherever possible) work together with Damian Conway's U<Perl Best Practices> and Jeffrey Thalhammer's Perl::Critic software.
+
+    L<http://shop.oreilly.com/product/9780596001735.do>
+
+    L<http://search.cpan.org/~thaljef/Perl-Critic/lib/Perl/Critic/PolicySummary.pod>
+
+=head3 The Book of RPerl
+
+    Perlism is the computer religion dedicated to the use, promotion, and development of the Perl family of programming languages.  (Not to be confused with a spiritual religion such as Christianity, a computer religion such as Perlism is an independent and complementary belief structure.)
+
+    A Perlite is an adherent to the Perlism religion.  Perlism has a revered founder, Saint Larry (himself a devout Christian); a prophet, The Voice In The Wilderness (Will); a monastary and shrine, Perl Monks; commandments, The LMPC; and canonical scriptures, including Saint Larry's U<Apocalypses> and The Voice's U<Book of RPerl>.
+
+    The book is a description of events surrounding the creation of RPerl and the future of the Internet.  It is intended to both educate and entertain.
+
+    L<http://rperl.org/the_book_of_rperl.html>
+
 =head2 Exercises
-=head2 LMPC
-=head2 history of RPerl
-=head2 Book of RPerl
-=head2 performance
+
+1.  Hello World
+2.  RPerl Command
+3.  Foo Bar Arithmetic
 
 
 =head1 APPENDIX A: EXERCISE ANSWERS
