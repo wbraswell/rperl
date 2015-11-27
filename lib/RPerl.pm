7246d3bd6368cbfa1ddeaa676b04be0820d5bc73
Will Braswell <william.braswell@autoparallel.com>
2015-11-27 04:43:52 -0600

    CPAN Release, v1.300000; Codename Pluto, Thanksgiving Edition

--- lib/RPerl.pm
+++ lib/RPerl.pm
@@ -4,10 +4,10 @@ use strict;
 use warnings;
 
 # DEV NOTE, CORRELATION #rp16: RPerl's underscore-is-comma (not CPAN's underscore-is-beta) numbering scheme utilized here
-our $VERSION = 1.200_002;    # CODENAME ANDROMEDA
+our $VERSION = 1.300_000;    # CODENAME PLUTO
 
-#our $VERSION = 20151107;    # NON-RELEASE VERSION NUMBER, OFFICIAL LONGDATE
-#our $VERSION = 2015.311;    # NON-RELEASE VERSION NUMBER, OFFICIAL STARDATE
+#our $VERSION = 20151126;    # NON-RELEASE VERSION NUMBER, OFFICIAL LONGDATE
+#our $VERSION = 2015.330;    # NON-RELEASE VERSION NUMBER, OFFICIAL STARDATE
 
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
