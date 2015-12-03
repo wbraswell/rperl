2b31b464b92efab2f90e8f24dfa56e978cbab370
Will Braswell <william.braswell@autoparallel.com>
2015-12-03 05:27:03 -0600

    Grammar, Operator Void, Print CPPOPS_CPPTYPES

--- lib/RPerl/Operation/Statement/OperatorVoid/Print.pm
+++ lib/RPerl/Operation/Statement/OperatorVoid/Print.pm
@@ -3,7 +3,7 @@ package RPerl::Operation::Statement::OperatorVoid::Print;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.002_000;
+our $VERSION = 0.003_100;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::Operation::Statement::OperatorVoid);
@@ -23,32 +23,41 @@ our string_hashref::method $ast_to_rperl__generate = sub {
     my string_hashref $rperl_source_group = { PMC => q{} };
     my string_hashref $rperl_source_subgroup;
 
-#    RPerl::diag( 'in OperatorVoid::Print->ast_to_rperl__generate(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
+    #    RPerl::diag( 'in OperatorVoid::Print->ast_to_rperl__generate(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
 
     my string $self_class = ref $self;
-    if ( $self_class eq 'OperatorVoid_116' ) { # OperatorVoid -> OP01_PRINT OPTIONAL-31 ListElements ';'
+    if ( $self_class eq 'OperatorVoid_116' ) {    # OperatorVoid -> OP01_PRINT OPTIONAL-31 ListElements ';'
         my string $print                  = $self->{children}->[0];
         my object $stdout_stderr_optional = $self->{children}->[1];
         my object $list_elements          = $self->{children}->[2];
         my string $semicolon              = $self->{children}->[3];
- 
+
 #        RPerl::diag( 'in OperatorVoid::Print->ast_to_rperl__generate(), have $stdout_stderr_optional = ' . "\n" . RPerl::Parser::rperl_ast__dump($stdout_stderr_optional) . "\n" );
 
+        if ( ( $stdout_stderr_optional->{children}->[0]->{attr} ne '{*STDOUT}' ) and ( $stdout_stderr_optional->{children}->[0]->{attr} ne '{*STDERR}' ) ) {
+            die RPerl::Parser::rperl_rule__replace(
+                'ERROR ECVGEASRP27, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: output stream '
+                    . $stdout_stderr_optional->{children}->[0]->{attr}
+                    . ' found where {*STDOUT} or {*STDERR} expected, dying'
+            ) . "\n";
+        }
+
         $rperl_source_group->{PMC} .= $print . q{ };
-        # DEV NOTE: STDOUT & STDERR are generated below, they are only grammar tokens, not grammar rules, so they do not get their own classes, the following do not exist:
-        # RPerl::InputOutput::Stderr & RPerl::InputOutput::Stdout
-        if (exists $stdout_stderr_optional->{children}->[0]) {
+
+# DEV NOTE: STDOUT & STDERR are generated below, they are only grammar tokens, not grammar rules, so they do not get their own classes, the following do not exist:
+# RPerl::InputOutput::Stderr & RPerl::InputOutput::Stdout
+        if ( exists $stdout_stderr_optional->{children}->[0] ) {
             $rperl_source_group->{PMC} .= $stdout_stderr_optional->{children}->[0]->{attr} . q{ };
         }
         $rperl_source_subgroup = $list_elements->ast_to_rperl__generate($modes);
         RPerl::Generator::source_group_append( $rperl_source_group, $rperl_source_subgroup );
         $rperl_source_group->{PMC} .= $semicolon . "\n";
     }
-    elsif ( $self_class eq 'OperatorVoid_117' ) { # OperatorVoid -> OP01_PRINT FHREF_SYMBOL_BRACES ListElements ';'
-        my string $print                  = $self->{children}->[0];
+    elsif ( $self_class eq 'OperatorVoid_117' ) {    # OperatorVoid -> OP01_PRINT FHREF_SYMBOL_BRACES ListElements ';'
+        my string $print               = $self->{children}->[0];
         my string $fhref_symbol_braces = $self->{children}->[1];
-        my object $list_elements          = $self->{children}->[2];
-        my string $semicolon              = $self->{children}->[3];
+        my object $list_elements       = $self->{children}->[2];
+        my string $semicolon           = $self->{children}->[3];
 
         $rperl_source_group->{PMC} .= $print . q{ } . $fhref_symbol_braces . q{ };
         $rperl_source_subgroup = $list_elements->ast_to_rperl__generate($modes);
@@ -56,11 +65,10 @@ our string_hashref::method $ast_to_rperl__generate = sub {
         $rperl_source_group->{PMC} .= $semicolon . "\n";
     }
     else {
-        die RPerl::Parser::rperl_rule__replace(
-            'ERROR ECVGEASRP00, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: grammar rule '
+        die RPerl::Parser::rperl_rule__replace( 'ERROR ECVGEASRP00, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: grammar rule '
                 . $self_class
-                . ' found where OperatorVoid_116 or OperatorVoid_117 expected, dying'
-        ) . "\n";
+                . ' found where OperatorVoid_116 or OperatorVoid_117 expected, dying' )
+            . "\n";
     }
 
     return $rperl_source_group;
@@ -68,10 +76,7 @@ our string_hashref::method $ast_to_rperl__generate = sub {
 
 our string_hashref::method $ast_to_cpp__generate__CPPOPS_PERLTYPES = sub {
     ( my object $self, my string_hashref $modes) = @_;
-    my string_hashref $cpp_source_group
-        = { CPP =>
-            q{// <<< RP::O::S::OV::P __DUMMY_SOURCE_CODE CPPOPS_PERLTYPES >>>}
-            . "\n" };
+    my string_hashref $cpp_source_group = { CPP => q{// <<< RP::O::S::OV::P __DUMMY_SOURCE_CODE CPPOPS_PERLTYPES >>>} . "\n" };
 
     #...
     return $cpp_source_group;
@@ -79,12 +84,50 @@ our string_hashref::method $ast_to_cpp__generate__CPPOPS_PERLTYPES = sub {
 
 our string_hashref::method $ast_to_cpp__generate__CPPOPS_CPPTYPES = sub {
     ( my object $self, my string_hashref $modes) = @_;
-    my string_hashref $cpp_source_group
-        = { CPP =>
-            q{// <<< RP::O::S::OV::P __DUMMY_SOURCE_CODE CPPOPS_CPPTYPES >>>}
-            . "\n" };
+    my string_hashref $cpp_source_group = { CPP => q{} };
+    my string_hashref $cpp_source_subgroup;
 
-    #...
+  #    RPerl::diag( 'in OperatorVoid::Print->ast_to_cpp__generate__CPPOPS_CPPTYPES(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
+
+    my string $self_class = ref $self;
+    if ( $self_class eq 'OperatorVoid_116' ) {    # OperatorVoid -> OP01_PRINT OPTIONAL-31 ListElements ';'
+        my object $stdout_stderr_optional = $self->{children}->[1];
+        my object $list_elements          = $self->{children}->[2];
+        my string $semicolon              = $self->{children}->[3];
+
+#        RPerl::diag( 'in OperatorVoid::Print->ast_to_cpp__generate__CPPOPS_CPPTYPES(), have $stdout_stderr_optional = ' . "\n" . RPerl::Parser::rperl_ast__dump($stdout_stderr_optional) . "\n" );
+
+        if ( exists $stdout_stderr_optional->{children}->[0] ) {
+            if ( $stdout_stderr_optional->{children}->[0]->{attr} eq '{*STDOUT}' ) {
+                $cpp_source_group->{CPP} .= 'cout << ';
+            }
+            elsif ( $stdout_stderr_optional->{children}->[0]->{attr} eq '{*STDERR}' ) {
+                $cpp_source_group->{CPP} .= 'cerr << ';
+            }
+            else {
+                die RPerl::Parser::rperl_rule__replace( 'ERROR ECVGEASCP27, CODE GENERATOR, ABSTRACT SYNTAX TO C++: output stream '
+                        . $stdout_stderr_optional->{children}->[0]->{attr}
+                        . ' found where {*STDOUT} or {*STDERR} expected, dying' )
+                    . "\n";
+            }
+        }
+        else {
+            $cpp_source_group->{CPP} .= 'cout << ';
+        }
+        $cpp_source_subgroup = $list_elements->ast_to_cpp__generate__CPPOPS_CPPTYPES($modes);
+        RPerl::Generator::source_group_append( $cpp_source_group, $cpp_source_subgroup );
+        $cpp_source_group->{CPP} .= $semicolon . "\n";
+    }
+    elsif ( $self_class eq 'OperatorVoid_117' ) {    # OperatorVoid -> OP01_PRINT FHREF_SYMBOL_BRACES ListElements ';'
+        $cpp_source_group->{CPP} .= '// <<< RP::O::S::OV::P __DUMMY_SOURCE_CODE CPPOPS_CPPTYPES >>>' . "\n";
+    }
+    else {
+        die RPerl::Parser::rperl_rule__replace( 'ERROR ECVGEASCP00, CODE GENERATOR, ABSTRACT SYNTAX TO C++: grammar rule '
+                . $self_class
+                . ' found where OperatorVoid_116 or OperatorVoid_117 expected, dying' )
+            . "\n";
+    }
+
     return $cpp_source_group;
 };
 
