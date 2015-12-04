4eec6804e7f3a6a2d4f2f6d85ceef7c55dbca6a1
Will Braswell <william.braswell@autoparallel.com>
2015-12-03 21:57:53 -0600

    Grammar, Operator, Equal Not Equal CPPOPS_CPPTYPES

--- lib/RPerl/Operation/Expression/Operator/Compare/EqualNotEqual.pm
+++ lib/RPerl/Operation/Expression/Operator/Compare/EqualNotEqual.pm
@@ -3,7 +3,7 @@ package RPerl::Operation::Expression::Operator::Compare::EqualNotEqual;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.001_000;
+our $VERSION = 0.002_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::Operation::Expression::Operator::Compare);
@@ -22,10 +22,21 @@ our string_hashref::method $ast_to_rperl__generate = sub {
     ( my object $self, my string_hashref $modes) = @_;
     my string_hashref $rperl_source_group = { PMC => q{} };
 
-#    RPerl::diag( 'in Operator::Compare::EqualNotEqual->ast_to_rperl__generate(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
+   #    RPerl::diag( 'in Operator::Compare::EqualNotEqual->ast_to_rperl__generate(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
 
     my string $self_class = ref $self;
-    if ( $self_class eq 'Operator_101' ) {  # Operator -> SubExpression OP12_COMPARE_EQ_NE SubExpression
+    if ( $self_class eq 'Operator_101' ) {    # Operator -> SubExpression OP12_COMPARE_EQ_NE SubExpression
+        if (    ( $self->{children}->[1] ne '==' )
+            and ( $self->{children}->[1] ne '!=' )
+            and ( $self->{children}->[1] ne 'eq' )
+            and ( $self->{children}->[1] ne 'ne' ) )
+        {
+            die RPerl::Parser::rperl_rule__replace( 'ERROR ECVGEASRP28, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: comparison operator '
+                    . $self->{children}->[1]
+                    . ' found where ==, !=, eq, or ne expected, dying' )
+                . "\n";
+        }
+
         my string_hashref $rperl_source_subgroup = $self->{children}->[0]->ast_to_rperl__generate($modes);
         RPerl::Generator::source_group_append( $rperl_source_group, $rperl_source_subgroup );
         $rperl_source_group->{PMC} .= q{ } . $self->{children}->[1] . q{ };
@@ -34,9 +45,7 @@ our string_hashref::method $ast_to_rperl__generate = sub {
     }
     else {
         die RPerl::Parser::rperl_rule__replace(
-            'ERROR ECVGEASRP00, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: grammar rule '
-                . $self_class
-                . ' found where Operator_101 expected, dying' )
+            'ERROR ECVGEASRP00, CODE GENERATOR, ABSTRACT SYNTAX TO RPERL: grammar rule ' . $self_class . ' found where Operator_101 expected, dying' )
             . "\n";
     }
 
@@ -45,11 +54,7 @@ our string_hashref::method $ast_to_rperl__generate = sub {
 
 our string_hashref::method $ast_to_cpp__generate__CPPOPS_PERLTYPES = sub {
     ( my object $self, my string_hashref $modes) = @_;
-    my string_hashref $cpp_source_group
-        = {
-        CPP => q{// <<< RP::O::E::O::C::ENE __DUMMY_SOURCE_CODE CPPOPS_PERLTYPES >>>}
-            . "\n"
-        };
+    my string_hashref $cpp_source_group = { CPP => q{// <<< RP::O::E::O::C::ENE __DUMMY_SOURCE_CODE CPPOPS_PERLTYPES >>>} . "\n" };
 
     #...
     return $cpp_source_group;
@@ -57,13 +62,38 @@ our string_hashref::method $ast_to_cpp__generate__CPPOPS_PERLTYPES = sub {
 
 our string_hashref::method $ast_to_cpp__generate__CPPOPS_CPPTYPES = sub {
     ( my object $self, my string_hashref $modes) = @_;
-    my string_hashref $cpp_source_group
-        = {
-        CPP => q{// <<< RP::O::E::O::C::ENE __DUMMY_SOURCE_CODE CPPOPS_CPPTYPES >>>}
-            . "\n"
-        };
+    my string_hashref $cpp_source_group = { CPP => q{} };
 
-    #...
+#    RPerl::diag( 'in Operator::Compare::EqualNotEqual->ast_to_cpp__generate__CPPOPS_CPPTYPES(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
+
+    my string $self_class = ref $self;
+    if ( $self_class eq 'Operator_101' ) {    # Operator -> SubExpression OP12_COMPARE_EQ_NE SubExpression
+        my string_hashref $cpp_source_subgroup = $self->{children}->[0]->ast_to_cpp__generate__CPPOPS_CPPTYPES($modes);
+        RPerl::Generator::source_group_append( $cpp_source_group, $cpp_source_subgroup );
+        if ( ( $self->{children}->[1] eq '==' ) or ( $self->{children}->[1] eq '!=' ) ) {
+            $cpp_source_group->{CPP} .= q{ } . $self->{children}->[1] . q{ };
+        }
+        elsif ( $self->{children}->[1] eq 'eq' ) {
+            $cpp_source_group->{CPP} .= ' == ';    # DEV NOTE: '==' is an overload for std::string::compare()
+        }
+        elsif ( $self->{children}->[1] eq 'ne' ) {
+            $cpp_source_group->{CPP} .= ' != ';    # DEV NOTE: '!=' is an overload for not(std::string::compare())
+        }
+        else {
+            die RPerl::Parser::rperl_rule__replace( 'ERROR ECVGEASCP28, CODE GENERATOR, ABSTRACT SYNTAX TO C++: comparison operator '
+                    . $self->{children}->[1]
+                    . ' found where ==, !=, eq, or ne expected, dying' )
+                . "\n";
+        }
+        $cpp_source_subgroup = $self->{children}->[2]->ast_to_cpp__generate__CPPOPS_CPPTYPES($modes);
+        RPerl::Generator::source_group_append( $cpp_source_group, $cpp_source_subgroup );
+    }
+    else {
+        die RPerl::Parser::rperl_rule__replace(
+            'ERROR ECVGEASCP00, CODE GENERATOR, ABSTRACT SYNTAX TO C++: grammar rule ' . $self_class . ' found where Operator_101 expected, dying' )
+            . "\n";
+    }
+
     return $cpp_source_group;
 };
 
