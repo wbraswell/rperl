fa0d3f4f64cc6e7aa79c488f943c96d6c2462c19
93r <ma_ko@riseup.net>
2016-08-12 21:29:49 +0200

    Operator, NamedSineCosine, CPPOPS_CPPTYPES for Literals

--- lib/RPerl/Operation/Expression/Operator/NamedUnary/Cosine.pm
+++ lib/RPerl/Operation/Expression/Operator/NamedUnary/Cosine.pm
@@ -8,7 +8,7 @@ package RPerl::Operation::Expression::Operator::NamedUnary::Cosine;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.002_000;
+our $VERSION = 0.003_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::Operation::Expression::Operator::NamedUnary);
@@ -72,10 +72,53 @@ our string_hashref::method $ast_to_cpp__generate__CPPOPS_PERLTYPES = sub {
 };
 
 our string_hashref::method $ast_to_cpp__generate__CPPOPS_CPPTYPES = sub {
-    ( my object $self, my string_hashref $modes) = @_;
-    my string_hashref $cpp_source_group = { CPP => q{// <<< RP::O::E::O::NU::C __DUMMY_SOURCE_CODE CPPOPS_CPPTYPES >>>} . "\n" };
+    ( my object $self, my object $operator_named, my string_hashref $modes) = @_;
+    my string_hashref $cpp_source_group = { CPP => q{} };
 
-    #...
+    RPerl::diag( 'in NamedUnary::Scalar->ast_to_cpp__generate__CPPOPS_CPPTYPES(), received $self = ' . "\n" . RPerl::Parser::rperl_ast__dump($self) . "\n" );
+    RPerl::diag( 'in NamedUnary::Scalar->ast_to_cpp__generate__CPPOPS_CPPTYPES(), received $operator_named = ' . "\n" . RPerl::Parser::rperl_ast__dump($operator_named) . "\n");
+
+    my string $operator_named_class = ref $operator_named;
+
+    if ( $operator_named_class eq 'Operation_81' ) {    # Operation -> OP10_NAMED_UNARY_SCOLON
+	die RPerl::Parser::rperl_rule__replace( 'ERROR EC0GEASCP16, CODE GENERATOR, ABSTRACT SYNTAX TO C++: Named operator '
+	    	. q{'} . $operator_named->{children}->[0] . q{'}
+		. ' requires exactly one argument, dying' )
+	    . "\n";
+    }
+    elsif ( $operator_named_class eq 'Operator_100' ) {    # Operator -> OP10_NAMED_UNARY SubExpression
+	my object $subexpression       = $operator_named->{children}->[1];
+	my string $subexpression_class = ref $subexpression;
+
+	$cpp_source_group->{CPP} .= $operator_named->{children}->[0];
+	$cpp_source_group->{CPP} .= '(';
+	RPerl::diag( 'XXX - subexpression_class = ' . RPerl::Parser::rperl_ast__dump($subexpression_class) . "\n");
+	if ( $subexpression_class eq 'SubExpression_138' ) { # Dealing with a Literal
+	    RPerl::diag( 'XXX - X -- ' . $subexpression_class . "\n" );
+	    my object $subsubexpression = $subexpression->{children}->[0];
+	    my string $subsubexpression_class = ref $subsubexpression;
+	    RPerl::diag( 'XXX - X - X -- ' . $subsubexpression_class . "\n" );
+	    RPerl::diag( 'XXX - X - X -- subsubexpression_class = ' . RPerl::Parser::rperl_ast__dump($subsubexpression_class) . "\n");
+	    if ( $subsubexpression_class eq 'Literal_235' ) { # Dealing with type Number
+		RPerl::diag( 'XXX - X - X -- subsubexpression = ' . "\n" . RPerl::Parser::rperl_ast__dump($subsubexpression) . "\n");
+		$cpp_source_group->{CPP} .= $subsubexpression->{children}->[0];
+	    }
+	}
+	$cpp_source_group->{CPP} .= ')';
+    }
+    elsif ( $operator_named_class eq 'Operator_101' ) {    # Operator -> OP10_NAMED_UNARY
+	die RPerl::Parser::rperl_rule__replace( 'ERROR EC0GEASCP16, CODE GENERATOR, ABSTRACT SYNTAX TO C++: Named operator '
+	    	. $operator_named->{children}->[0]
+		. ' requires exactly one argument, dying' )
+	    . "\n";
+    }
+    else {
+	die RPerl::Parser::rperl_rule__replace( 'ERROR EC0GEASCP00, CODE GENERATOR, ABSTRACT SYNTAX TO C++: Grammar rule '
+	    	. ($operator_named_class)
+		. ' found where Operation_81, Operator_100, or Operator_101 expected, dying' )
+	    . "\n";
+    }
+
     return $cpp_source_group;
 };
 
