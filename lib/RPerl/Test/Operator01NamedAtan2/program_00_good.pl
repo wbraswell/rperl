91bb96c3d907624ee5d20cf024f25d4d4c2b4851
93r <ma_ko@riseup.net>
2016-08-23 17:33:08 +0200

    Operator, Operator01NamedAtan2, Implement PERLOPS_PERLTYPES & CPPOPS_CPPTYPES

---
+++ lib/RPerl/Test/Operator01NamedAtan2/program_00_good.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# [[[ PREPROCESSOR ]]]
+# <<< EXECUTE_SUCCESS: 'have $op_atan2 = 3.141_592_653_589_79' >>>
+
+# [[[ HEADER ]]]
+use RPerl;
+use strict;
+use warnings;
+our $VERSION = 0.001_000;
+
+# [[[ CRITICS ]]]
+## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
+## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
+
+# [[[ OPERATIONS ]]]
+my number $y = 0;
+my number $x = -1;
+my number $op_atan2 = (atan2 $y, $x);
+
+print 'have $op_atan2 = ', number_to_string($op_atan2), "\n";
