eb3d80c93055ec265efb141d25cc593b848e8dc6
Will Braswell <william.braswell@autoparallel.com>
2017-04-28 22:45:37 -0500

    Fix Recursive String & HelperFuncs Deps, Part 9

--- lib/RPerl/DataType/String.pm
+++ lib/RPerl/DataType/String.pm
@@ -3,7 +3,7 @@ package RPerl::DataType::String;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.008_000;
+our $VERSION = 0.009_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::DataType::Scalar);
@@ -41,23 +41,13 @@ use warnings;
 # [[[ INCLUDES ]]]
 use POSIX qw(floor);
 
-#BEGIN { print '<<< DEBUG String.pm 0a0 >>>', "\n"; }
-#BEGIN { print '<<< DEBUG String.pm 0a0 >>>, have @ARGV = ', Dumper(\@ARGV), "\n"; }
-#BEGIN { print '<<< DEBUG String.pm 0a0 >>>, have $ARGV[0] = ', $ARGV[0], "\n"; }
-BEGIN { print '<<< DEBUG String.pm 0a0 >>>, have (substr $ARGV[0], -7, 7) = ', (substr $ARGV[0], -7, 7), "\n"; }
 # DEV NOTE: do not put inside INIT{} block, because it will be "too late to run INIT block" in some cases, such as inside Catalyst
 
-#if ((not exists $main::{'RPerl::SvPOKp'} ) or (not defined &{ $main::{'RPerl::SvPOKp'} })) {
-#if (not $RPerl::HelperFunctions_cpp::LOADING) {
-#if ((not exists $ENV{RPERL_HELPERS_LOADING}) or (not defined $ENV{RPERL_HELPERS_LOADING}) or (not $ENV{RPERL_HELPERS_LOADING})) {
+# DEV NOTE, CORRELATION #rp040: fix recursive dependencies of String.pm & HelperFunctions_cpp.pm, as triggered by ingy's Inline::create_config_file() `system` call
 if (not ((exists $ARGV[0]) and (defined $ARGV[0]) and ((substr $ARGV[0], -7, 7) eq '_Inline'))) {
-BEGIN { print '<<< DEBUG String.pm 0a1 >>>', "\n"; }
     use RPerl::HelperFunctions_cpp;  # main::RPerl_SvPOKp
-#BEGIN { print '<<< DEBUG String.pm 0a2, have $RPerl::HelperFunctions_cpp::LOADING = ', $RPerl::HelperFunctions_cpp::LOADING, ' >>>', "\n"; }
-BEGIN { print '<<< DEBUG String.pm 0a2, have $ENV{RPERL_HELPERS_LOADING} = ', $ENV{RPERL_HELPERS_LOADING}, ' >>>', "\n"; }
     RPerl::HelperFunctions_cpp::cpp_load();
 }
-BEGIN { print '<<< DEBUG String.pm 0a3 >>>', "\n"; }
 
 # [[[ EXPORTS ]]]
 use Exporter 'import';
