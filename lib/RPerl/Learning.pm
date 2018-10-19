6e088b96aaa3f0a19d43e1d7dcca3b1fd524455a
Will Braswell <william.braswell@autoparallel.com>
2018-10-19 16:43:15 -0500

    Learning RPerl, Update Copyright Year 2018

--- lib/RPerl/Learning.pm
+++ lib/RPerl/Learning.pm
@@ -3,7 +3,7 @@ use RPerl;
 package RPerl::Learning;
 use strict;
 use warnings;
-our $VERSION = 0.193_000;
+our $VERSION = 0.194_000;
 
 # [[[ OO INHERITANCE ]]]
 # NEED FIX: why does the following 'use parent' command cause $VERSION to become undefined???
@@ -66,7 +66,7 @@ Learning RPerl
 
 =head1 COPYRIGHT
 
-Learning RPerl is Copyright © 2013, 2014, 2015, 2016, 2017 William N. Braswell, Jr.
+Learning RPerl is Copyright © 2013, 2014, 2015, 2016, 2017, 2018 William N. Braswell, Jr.
 All Rights Reserved.
 
 Learning RPerl is part of the RPerl Family of software and documentation.
