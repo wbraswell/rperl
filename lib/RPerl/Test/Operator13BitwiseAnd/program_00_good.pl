e485261a36fbfaf7630850cd68f1c075326fc110
shiv.si <shiv.si@shivsi.local>
2019-05-01 14:53:18 +0530

    good tests upgraded to include unsigned for bitwise and

--- lib/RPerl/Test/Operator13BitwiseAnd/program_00_good.pl
+++ lib/RPerl/Test/Operator13BitwiseAnd/program_00_good.pl
@@ -16,17 +16,33 @@ my string $bar  = 'howdy';
 my integer $bat = $foo & 22;
 my integer $baz = 9_123.456_789 & $foo;
 my integer $bax = 33 & 44;
+my unsigned_integer $unsignedfoo = 0;
+my unsigned_integer $unsignedbar = 1;
+my unsigned_integer $unsignedbat = $unsignedfoo & 22 ;
+my unsigned_integer $unsignedbaz = $unsignedfoo & $unsignedbar ;
 
 print 'have $foo = ', $foo, "\n";
 print 'have $bar = ', $bar, "\n";
 print 'have $bat = ', $bat, "\n";
 print 'have $baz = ', $baz, "\n";
 print 'have $bax = ', $bax, "\n";
+print 'have $unsignedfoo = ', $unsignedfoo, "\n";
+print 'have $unsignedbar = ', $unsignedbar, "\n";
+print 'have $unsignedbat = ', $unsignedbat, "\n";
+print 'have $unsignedbaz = ', $unsignedbaz, "\n";
 
 $bat = $bar & 'abc';
 $baz = '444' & '222';
 $bax = '33' & $bar;
+$unsignedfoo = 2;
+$unsignedbar = 3;
+$unsignedbat = $unsignedfoo & 33;
+$unsignedbaz = $unsignedfoo & $unsignedbar ;
 
 print 'have $bat = ', $bat, "\n";
 print 'have $baz = ', $baz, "\n";
 print 'have $bax = ', $bax, "\n";
+print 'have $unsignedfoo = ', $unsignedfoo, "\n";
+print 'have $unsignedbar = ', $unsignedbar, "\n";
+print 'have $unsignedbat = ', $unsignedbat, "\n";
+print 'have $unsignedbaz = ', $unsignedbaz, "\n";
